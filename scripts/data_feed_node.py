5adeafbf5a59cff2b48a5f9f1e439a0556864da4
Ho Hoan Nghia <hoannghia_ho@sutd.edu.sg>
2022-10-21 14:38:30 +0800

    first commit

---
+++ scripts/data_feed_node.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import rospy
+import message_filters
+
+from sensor_msgs.msg import Image, PointCloud2
+from draconis_demo_custom_msgs.msg import ImagePointcloudMsg
+
+# from timeit import default_timer as timer 
+
+
+__NODE_NAME = "data_feed_node"
+
+
+class DataFeedNode():
+    def __init__(self) -> None:
+        
+        self.__cameraTopic = rospy.get_param("~camera_input_topic")
+        self.__lidarTopic = rospy.get_param("~lidar_input_topic")
+        self.__imageCloudOutputTopic = rospy.get_param("~image_cloud_output_topic")
+        self.__outputMinPeriod = 1 / rospy.get_param("~output_framerate", 2)
+
+        rospy.loginfo(f'{self.__cameraTopic}')
+
+        self.__prevTime = rospy.Time.now()
+
+        self.__imageCloudPackage = ImagePointcloudMsg()
+
+        self.__imageFilter = message_filters.Subscriber(self.__cameraTopic, Image)
+        self.__cloudFilter = message_filters.Subscriber(self.__lidarTopic, PointCloud2)
+        self.__sync = message_filters.ApproximateTimeSynchronizer([self.__imageFilter, self.__cloudFilter], \
+                                                                  queue_size=5, \
+                                                                  slop=0.3)
+        self.__sync.registerCallback(self.__image_cloud_receiving_callback)
+
+        self.__imageCloudPub = rospy.Publisher(self.__imageCloudOutputTopic, ImagePointcloudMsg, queue_size=5, tcp_nodelay=True)
+
+
+    def __image_cloud_receiving_callback(self, image: Image, cloud: PointCloud2 ):
+        
+        #--- do timing 
+        elapsedTime = rospy.Time.now() - self.__prevTime
+
+        if (elapsedTime.to_sec() < self.__outputMinPeriod):
+            return
+
+        self.__prevTime = rospy.Time.now()
+
+        #--- get data and publish
+        self.__imageCloudPackage.image = image
+        self.__imageCloudPackage.pointcloud = cloud
+
+        self.__imageCloudPub.publish(self.__imageCloudPackage)
+
+
+    def onShutDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    rospy.init_node(__NODE_NAME)
+    rospy.loginfo("Start " + __NODE_NAME)
+
+    __node = DataFeedNode()
+
+    rospy.on_shutdown(__node.onShutDown)
+    rospy.spin()   
