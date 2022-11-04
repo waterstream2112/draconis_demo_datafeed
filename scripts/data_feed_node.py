9988f80c35f125234a81b8950b2c7076f7c2616c
Ho Hoan Nghia <hoannghia_ho@sutd.edu.sg>
2022-11-04 15:33:59 +0800

    change camera to argus

--- scripts/data_feed_node.py
+++ scripts/data_feed_node.py
@@ -17,6 +17,7 @@ class DataFeedNode():
         
         self.__cameraTopic = rospy.get_param("~camera_input_topic")
         self.__lidarTopic = rospy.get_param("~lidar_input_topic")
+        self.__modifiedImageOutputTopic = rospy.get_param("~modified_argus_front_right_output_topic")
         self.__imageCloudOutputTopic = rospy.get_param("~image_cloud_output_topic")
         self.__outputMinPeriod = 1 / rospy.get_param("~output_framerate", 2)
 
@@ -26,17 +27,22 @@ class DataFeedNode():
 
         self.__imageCloudPackage = ImagePointcloudMsg()
 
-        self.__imageFilter = message_filters.Subscriber(self.__cameraTopic, Image)
+        self.__imageFilter = message_filters.Subscriber(self.__modifiedImageOutputTopic, Image)
         self.__cloudFilter = message_filters.Subscriber(self.__lidarTopic, PointCloud2)
         self.__sync = message_filters.ApproximateTimeSynchronizer([self.__imageFilter, self.__cloudFilter], \
                                                                   queue_size=5, \
                                                                   slop=0.3)
         self.__sync.registerCallback(self.__image_cloud_receiving_callback)
 
-        self.__imageCloudPub = rospy.Publisher(self.__imageCloudOutputTopic, ImagePointcloudMsg, queue_size=5, tcp_nodelay=True)
+        self.__imageSub = rospy.Subscriber(self.__cameraTopic, Image, self.__image_callback, queue_size=5, tcp_nodelay=True)
+        self.__cloudSub = rospy.Subscriber(self.__lidarTopic, PointCloud2, self.__cloud_callback, queue_size=5, tcp_nodelay=True)
 
+        self.__imageCloudPub = rospy.Publisher(self.__imageCloudOutputTopic, ImagePointcloudMsg, queue_size=5, tcp_nodelay=True)
+        self.__modifiedImagePub = rospy.Publisher(self.__modifiedImageOutputTopic, Image, queue_size=5, tcp_nodelay=True)
 
     def __image_cloud_receiving_callback(self, image: Image, cloud: PointCloud2 ):
+        
+        rospy.loginfo("__image_cloud_receiving_callback")
         
         #--- do timing 
         elapsedTime = rospy.Time.now() - self.__prevTime
@@ -52,6 +58,14 @@ class DataFeedNode():
 
         self.__imageCloudPub.publish(self.__imageCloudPackage)
 
+    def __image_callback(self, image: Image):
+        rospy.loginfo(f"__image_callback {(rospy.Time.now() - image.header.stamp).to_sec() : 0.2f}")
+
+        image.header.stamp = rospy.Time.now()
+        self.__modifiedImagePub.publish(image)
+
+    def __cloud_callback(self, cloud: PointCloud2):
+        rospy.loginfo(f"__cloud_callback {(rospy.Time.now() - cloud.header.stamp).to_sec() : 0.2f}")
 
     def onShutDown(self):
         pass
