1c2c146c62f488e785cf9aac1d886fc41b3d1917
Ho Hoan Nghia <hoannghia_ho@sutd.edu.sg>
2022-11-21 10:54:25 +0800

    update vision_perception

--- scripts/node_data_for_vision_perception.py
+++ scripts/node_data_for_vision_perception.py
@@ -2,9 +2,13 @@
 
 import rospy
 import message_filters
+import tf2_ros
 
 from sensor_msgs.msg import Image, PointCloud2
+from geometry_msgs.msg import TransformStamped
 from draconis_demo_custom_msgs.msg import ImagePointcloudMsg
+
+
 
 # from timeit import default_timer as timer 
 
@@ -20,12 +24,18 @@ class NodeDataForVisionPerception():
         self.__modifiedImageOutputTopic = rospy.get_param("~modified_argus_front_right_output_topic")
         self.__imageCloudOutputTopic = rospy.get_param("~image_cloud_output_topic")
         self.__outputMinPeriod = 1 / rospy.get_param("~output_framerate", 2)
+        self.__imageCloudPackageFrameId =  rospy.get_param("~image_cloud_package_frame_id")
 
         rospy.loginfo(f'{self.__cameraTopic}')
+        rospy.loginfo(f'{self.__imageCloudOutputTopic}')
 
         self.__prevTime = rospy.Time.now()
 
         self.__imageCloudPackage = ImagePointcloudMsg()
+
+        #--- tf2 
+        self.__tfBuffer = tf2_ros.Buffer()
+        self.__listener = tf2_ros.TransformListener(self.__tfBuffer)
 
         # self.__imageFilter = message_filters.Subscriber(self.__modifiedImageOutputTopic, Image)
         self.__imageFilter = message_filters.Subscriber(self.__cameraTopic, Image)
@@ -54,9 +64,18 @@ class NodeDataForVisionPerception():
 
         self.__prevTime = rospy.Time.now()
 
+        #--- get TransformStamped
+        self.__trans = TransformStamped()
+        try:
+            self.__trans = self.__tfBuffer.lookup_transform(self.__imageCloudPackageFrameId, cloud.header.frame_id, cloud.header.stamp)
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.logerr("__image_cloud_receiving_callback, failed to get transform")
+            return
+
         #--- get data and publish
         self.__imageCloudPackage.image = image
         self.__imageCloudPackage.pointcloud = cloud
+        self.__imageCloudPackage.transform_stamped = self.__trans
 
         self.__imageCloudPub.publish(self.__imageCloudPackage)
 
