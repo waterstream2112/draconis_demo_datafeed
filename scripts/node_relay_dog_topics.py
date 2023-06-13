9f9e9b03218663df00ce5710a73a82f7d0cb01dc
Ho Hoan Nghia <waterstream2112@gmail.com>
2023-06-13 17:51:27 +0800

    add some relay node_relay_dog_topics

--- scripts/node_relay_dog_topics.py
+++ scripts/node_relay_dog_topics.py
@@ -7,6 +7,8 @@ import tf2_ros
 from std_msgs.msg import UInt8MultiArray
 from sensor_msgs.msg import Image, PointCloud2
 from geometry_msgs.msg import TransformStamped
+from draconis_demo_custom_msgs.msg import ObjectDetectionMsg
+from visualization_msgs.msg import MarkerArray
 
 
 __NODE_NAME = "node_relay_dog_topics"
@@ -22,14 +24,18 @@ class NodeRelayDogTopics():
                               rospy.get_param("~topic_input_ar0234_front_right", ""), \
                               rospy.get_param("~topic_input_ar0234_side_left", ""), \
                               rospy.get_param("~topic_input_ar0234_side_right", ""), \
-                              rospy.get_param("~topic_input_ar0234_rear", "")
+                              rospy.get_param("~topic_input_ar0234_rear", ""), \
+                              rospy.get_param("~topic_input_object_detection", ""), \
+                              rospy.get_param("~topic_input_obstacle_edge_cloud", "")
                              ]
 
         self.__topicOutputs = [rospy.get_param("~topic_output_state_heartbeat", ""), \
                               rospy.get_param("~topic_output_ar0234_front_right", ""), \
                               rospy.get_param("~topic_output_ar0234_side_left", ""), \
                               rospy.get_param("~topic_output_ar0234_side_right", ""), \
-                              rospy.get_param("~topic_output_ar0234_rear", "")
+                              rospy.get_param("~topic_output_ar0234_rear", ""), \
+                              rospy.get_param("~topic_output_object_detection", ""), \
+                              rospy.get_param("~topic_output_obstacle_edge_cloud", "")
                               ]
 
         #--- Subscribers
@@ -37,7 +43,9 @@ class NodeRelayDogTopics():
                               rospy.Subscriber(self.__topicInputs[1], Image, self.__callback_1, queue_size=5, tcp_nodelay=True), \
                               rospy.Subscriber(self.__topicInputs[2], Image, self.__callback_2, queue_size=5, tcp_nodelay=True), \
                               rospy.Subscriber(self.__topicInputs[3], Image, self.__callback_3, queue_size=5, tcp_nodelay=True), \
-                              rospy.Subscriber(self.__topicInputs[4], Image, self.__callback_4, queue_size=5, tcp_nodelay=True)
+                              rospy.Subscriber(self.__topicInputs[4], Image, self.__callback_4, queue_size=5, tcp_nodelay=True), \
+                              rospy.Subscriber(self.__topicInputs[5], ObjectDetectionMsg, self.__callback_5, queue_size=5, tcp_nodelay=True), \
+                              rospy.Subscriber(self.__topicInputs[6], MarkerArray, self.__callback_6, queue_size=5, tcp_nodelay=True)
                              ]
 
         #--- Publishers
@@ -45,15 +53,19 @@ class NodeRelayDogTopics():
                              rospy.Publisher(self.__topicOutputs[1], Image, queue_size=5, tcp_nodelay=True), \
                              rospy.Publisher(self.__topicOutputs[2], Image, queue_size=5, tcp_nodelay=True), \
                              rospy.Publisher(self.__topicOutputs[3], Image, queue_size=5, tcp_nodelay=True), \
-                             rospy.Publisher(self.__topicOutputs[4], Image, queue_size=5, tcp_nodelay=True)
+                             rospy.Publisher(self.__topicOutputs[4], Image, queue_size=5, tcp_nodelay=True), \
+                             rospy.Publisher(self.__topicOutputs[5], ObjectDetectionMsg, queue_size=5, tcp_nodelay=True), \
+                             rospy.Publisher(self.__topicOutputs[6], MarkerArray, queue_size=5, tcp_nodelay=True)
                             ]
 
 
     def __callback_0(self, data: UInt8MultiArray):
+        # rospy.loginfo("<< node::__callback_0 >>")
         self.__publishers[0].publish(data)
 
 
     def __callback_1(self, image: Image):
+        # rospy.loginfo("<< node::__callback_1 >>")
         self.__publishers[1].publish(image)
 
 
@@ -67,6 +79,13 @@ class NodeRelayDogTopics():
     
     def __callback_4(self, image: Image):
         self.__publishers[4].publish(image)
+        
+
+    def __callback_5(self, data: ObjectDetectionMsg):
+        self.__publishers[5].publish(data)
+
+    def __callback_6(self, data: MarkerArray):
+        self.__publishers[6].publish(data)
         
 
     def onShutDown(self):
